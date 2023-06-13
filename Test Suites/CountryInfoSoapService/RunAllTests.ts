382fab1c2d07e5ec2c9cef48e0115fc79697f894
nikkapallado <ntpallado@gmail.com>
2023-06-13 22:38:22 +0800

    feat: add test suite for country info soap service

---
+++ Test Suites/CountryInfoSoapService/RunAllTests.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RunAllTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>042d746f-4c86-493c-a9b4-92f5d41602e6</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb6549f4-9a72-4f9e-b36f-6db5a46da85d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CountryInfoSoapTests/TestOne</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2e5a18c2-7ab4-4f3b-a09a-df840ec3d10a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CountryInfoSoapTests/APIChainingXML</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
